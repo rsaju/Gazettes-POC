b492bed7642b928e186b901819c1fce3054797e1
rsaju <43270726+rsaju@users.noreply.github.com>
2018-09-24 13:43:00 +0530

    Book class || model || created

---
+++ Db_Service/src/main/java/com/gazette/poc/Db_Service/Model/Book.java
@@ -0,0 +1,42 @@
+package com.gazette.poc.Db_Service.Model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="book_tbl")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int bid;
+
+    @Column(name="bookname")
+    private String bookname;
+
+    @Column(name="bookcount")
+    private String bookcount;
+
+    public int getBid() {
+        return bid;
+    }
+
+    public void setBid(int bid) {
+        this.bid = bid;
+    }
+
+    public String getBookname() {
+        return bookname;
+    }
+
+    public void setBookname(String bookname) {
+        this.bookname = bookname;
+    }
+
+    public String getBookcount() {
+        return bookcount;
+    }
+
+    public void setBookcount(String bookcount) {
+        this.bookcount = bookcount;
+    }
+}
