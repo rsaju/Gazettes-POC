a0efdd9f537e771dcbe8de770e1f00a51bb3a0dd
rsaju <43270726+rsaju@users.noreply.github.com>
2018-09-24 18:20:21 +0530

    @RepositoryRestResource added

--- Db_Service/src/main/java/com/gazette/poc/Db_Service/Service/BookService.java
+++ Db_Service/src/main/java/com/gazette/poc/Db_Service/Service/BookService.java
@@ -2,6 +2,8 @@ package com.gazette.poc.Db_Service.Service;
 
 import com.gazette.poc.Db_Service.Model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "book", path = "book")
 public interface BookService extends JpaRepository<Book,Integer> {
 }
