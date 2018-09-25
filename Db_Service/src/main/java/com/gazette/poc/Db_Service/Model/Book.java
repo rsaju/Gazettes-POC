ab9f7d21842f5089d5348cf438f266a157c17a98
Rajeshrajan1991 <rajeshr.r61@gmail.com>
2018-09-25 16:09:46 +0530

    bookname updated

--- Db_Service/src/main/java/com/gazette/poc/Db_Service/Model/Book.java
+++ Db_Service/src/main/java/com/gazette/poc/Db_Service/Model/Book.java
@@ -3,22 +3,29 @@ package com.gazette.poc.Db_Service.Model;
 import javax.persistence.*;
 
 @Entity
-@Table(name="book_tbl")
+@Table(name = "book_tbl")
 public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int bid;
 
-    @Column(name="bookname")
-    private String bookname;
+    @Column(name = "bookname")
+    private String bookName;
 
     @Column(name = "bookauthor")
     private String bookAuthor;
 
-    @Column(name="bookcount")
+    @Column(name = "bookcount")
+    private String bookCount;
 
-    private String bookcount;
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
 
     public String getBookAuthor() {
         return bookAuthor;
@@ -36,19 +43,12 @@ public class Book {
         this.bid = bid;
     }
 
-    public String getBookname() {
-        return bookname;
+    public String getbookName() {
+        return bookName;
     }
 
-    public void setBookname(String bookname) {
-        this.bookname = bookname;
+    public void setbookName(String bookname) {
+        this.bookName = bookname;
     }
 
-    public String getBookcount() {
-        return bookcount;
-    }
-
-    public void setBookcount(String bookcount) {
-        this.bookcount = bookcount;
-    }
 }
