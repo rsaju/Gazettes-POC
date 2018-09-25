d0958872fdd69ccb1ab93c79ddae4fb8230c447a
Rajeshrajan1991 <rajeshr.r61@gmail.com>
2018-09-25 16:10:20 +0530

    Method Name Updated

--- Db_Service/src/main/java/com/gazette/poc/Db_Service/Service/BookService.java
+++ Db_Service/src/main/java/com/gazette/poc/Db_Service/Service/BookService.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 public interface BookService extends JpaRepository<Book,Integer> {
 
-    List<Book> findBybookname(String name);
+    List<Book> findByBookName(String name);
 }
