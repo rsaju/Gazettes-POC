974c57fc70bf2b8273fa107c5d208b891a5faa2b
Rajeshrajan1991 <rajeshr.r61@gmail.com>
2018-09-25 15:40:56 +0530

    Method to delete book by Id

--- Db_Service/src/main/java/com/gazette/poc/Db_Service/Controller/BookController.java
+++ Db_Service/src/main/java/com/gazette/poc/Db_Service/Controller/BookController.java
@@ -5,15 +5,13 @@ import com.gazette.poc.Db_Service.Service.BookService;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.net.URLDecoder;
 import java.util.List;
 
 @RestController
@@ -25,11 +23,11 @@ public class BookController {
     Gson gson = new Gson();
 
     @GetMapping("/findBook/{bookname}")
-    public String searchBookByName(HttpServletRequest request ,@PathVariable("bookname") String bookname){
+    public String searchBookByName(HttpServletRequest request ,@PathVariable("bookname") String bookname) throws Exception{
 
-        List<Book> books = bookService.findByName(bookname);
+        String book = URLDecoder.decode(bookname, "UTF-8");
+        List<Book> books = bookService.findBybookName(bookname);
         String jsonBook = gson.toJson(books);
-//        ResponseEntity<String> responseEntity = new ResponseEntity<String>
         return jsonBook;
     }
 
@@ -46,6 +44,12 @@ public class BookController {
         Gson gson = new Gson();
         Book book = gson.fromJson(bufferedReader, Book.class);
         bookService.save(book);
+    }
+
+    @DeleteMapping("/deleteBook/{bookId}")
+    public String deleteBook(HttpServletRequest request, @PathVariable("bookId") int bookId) throws Exception {
+        bookService.deleteById(bookId);
+        return "Book deleteed with Id:"+bookId;
     }
 
 }
