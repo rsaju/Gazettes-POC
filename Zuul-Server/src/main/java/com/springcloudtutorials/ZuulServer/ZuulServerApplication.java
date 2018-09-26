300b3198e6b8157fddbdfdd454f3306e9a5a6b7a
rsaju <43270726+rsaju@users.noreply.github.com>
2018-09-26 19:16:20 +0530

    Signed-off-by: rsaju <43270726+rsaju@users.noreply.github.com>

--- Zuul-Server/src/main/java/com/springcloudtutorials/ZuulServer/ZuulServerApplication.java
+++ Zuul-Server/src/main/java/com/springcloudtutorials/ZuulServer/ZuulServerApplication.java
@@ -2,8 +2,10 @@ package com.springcloudtutorials.ZuulServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulServerApplication {
 
 	public static void main(String[] args) {
