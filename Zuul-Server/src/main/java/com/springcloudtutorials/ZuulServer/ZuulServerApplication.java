8aaab295a3befe86d213d0bbeb8facfdeeae40e8
Aswin Prabhakar <aswinprabhakar1758@gmail.com>
2018-09-27 17:18:00 +0530

    Added discovery client annotations

--- Zuul-Server/src/main/java/com/springcloudtutorials/ZuulServer/ZuulServerApplication.java
+++ Zuul-Server/src/main/java/com/springcloudtutorials/ZuulServer/ZuulServerApplication.java
@@ -1,14 +1,25 @@
 package com.springcloudtutorials.ZuulServer;
 
+import com.springcloudtutorials.ZuulServer.ZuulFilters.TestZuulFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient
+@Configuration
 public class ZuulServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ZuulServerApplication.class, args);
+	}
+
+	@Bean
+	public TestZuulFilter testZuulFilter(){
+		return new TestZuulFilter();
 	}
 }
