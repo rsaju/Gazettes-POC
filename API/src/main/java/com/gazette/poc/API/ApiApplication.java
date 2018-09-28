dcb86946c8d8692f46edb181b3a47d2bed29609d
Aswin Prabhakar <aswinprabhakar1758@gmail.com>
2018-09-28 11:36:29 +0530

    Creating Ribbon enabled restTemplate bean

--- API/src/main/java/com/gazette/poc/API/ApiApplication.java
+++ API/src/main/java/com/gazette/poc/API/ApiApplication.java
@@ -3,15 +3,27 @@ package com.gazette.poc.API;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RefreshScope
+@Configuration
 public class ApiApplication {
+
+		
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
 	}
 }
