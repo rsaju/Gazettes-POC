60f16256758d3902c1ad8c0b3e2183ebd70215dc
rsaju <43270726+rsaju@users.noreply.github.com>
2018-12-06 17:37:08 +0530

    config directory

--- API/src/main/java/com/gazette/poc/API/feignClientInterfaces/DbService.java
+++ API/src/main/java/com/gazette/poc/API/feignClientInterfaces/DbService.java
@@ -3,13 +3,16 @@ package com.gazette.poc.API.feignClientInterfaces;
 import com.gazette.poc.API.entities.UserDTO;
 import feign.Headers;
 import feign.RequestLine;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-//@FeignClient(name = "db-service", url = "http://db-service:8082")
-@Headers("Accept: application/json")
+@FeignClient(name = "db-service", url = "http://db-service:8082")
 public interface DbService {
 
-
-    @Headers("Content-Type: application/json")
-    @RequestLine("POST" + "/add_user")
-    public void registerUser(UserDTO user);
+    @PostMapping("/add_user")
+    @RequestMapping(method= RequestMethod.POST, value="/add_user", consumes = MediaType.)
+    public void registerUser(String user);
 }
