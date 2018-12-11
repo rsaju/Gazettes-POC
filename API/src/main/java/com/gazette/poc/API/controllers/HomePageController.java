48fc89e51a821f8801e02fa79c7e55eb3255a3f3
Aswin Prabhakar <aswinprabhakar1758@gmail.com>
2018-12-11 12:03:07 +0530

    Changes to load the homepage title directly from configmap

--- API/src/main/java/com/gazette/poc/API/controllers/HomePageController.java
+++ API/src/main/java/com/gazette/poc/API/controllers/HomePageController.java
@@ -1,14 +1,20 @@
 package com.gazette.poc.API.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomePageController {
 
-    @GetMapping("/")
-    public String showHomePage(){
+    @Value("${home.page.heading}")
+    private String heading;
 
+    @GetMapping("/")
+    public String showHomePage(Model model){
+
+        model.addAttribute("heading",heading);
         return "homepage";
     }
 }
